aebf0f7b6b5df19386e06ce8cdbc5609f54923ca
Sebastian906 <elsebas1912@gmail.com>
2025-08-01 15:53:02 -0500

    Adaptación de plantilla completa con diferentes paginas

--- Gin/routes/routes.go
+++ Gin/routes/routes.go
@@ -2,7 +2,8 @@ package routes
 
 import (
 	"net/http"
-
+	"os"
+	"strings"
 	"github.com/gin-gonic/gin"
 )
 
@@ -21,6 +22,18 @@ func SetupRoutes(r *gin.Engine) {
 
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	r.GET("/:page", func(c *gin.Context) {
+		page := c.Param("page")
+		if !strings.HasSuffix(page, ".html") {
+			page += ".html"
+		}
+		if _, err := os.Stat("templates/" + page); err == nil {
+			c.HTML(http.StatusOK, page, nil)
+		} else {
+			c.HTML(http.StatusNotFound, "404.html", nil)
+		}
 	})
 
 }
