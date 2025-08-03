a5cd1bb18e411e97f3e20234d48914b9d707a88f
Sebastian906 <elsebas1912@gmail.com>
2025-08-02 20:58:23 -0500

    Instalación de paquete de middleware para Echo

--- Echo/main.go
+++ Echo/main.go
@@ -4,10 +4,17 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	// e.Use(middleware.Logger())
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}, time=${latency_human}\n",
+	}))
 
 	e.GET("/", func(c echo.Context) error {
 		// return c.String(http.StatusOK, "Hola mundo")
