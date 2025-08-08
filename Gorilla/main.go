5ec65631f52ab0261a4b57733b7cd9a8aaf3bbbb
Sebastian906 <elsebas1912@gmail.com>
2025-08-07 20:26:07 -0500

    Función de concatenación de middleware y llamado a la función principal

--- Gorilla/main.go
+++ Gorilla/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -15,6 +16,8 @@ func main() {
 		fmt.Fprint(w, "¡Bienvenido al servidor Gorilla!")
 	})
 
+	r.Use(LogginMiddleware)
+
 	r.Use(func (next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			log.Println(r.Method, r.URL.Path)
@@ -24,4 +27,13 @@ func main() {
 
 	log.Println("Servidor gorilla en ejecución en http://localhost:8080")
 	http.ListenAndServe(":8080", r)
+}
+
+func LogginMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		latency := time.Since(start)
+		log.Printf("%s %s - %v - %s", r.Method, r.URL.Path, latency, http.StatusText(http.StatusOK))
+	})
 }
