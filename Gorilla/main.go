468969ecb7c6172002a1121b585d4257efd57014
Sebastian906 <elsebas1912@gmail.com>
2025-08-07 19:56:51 -0500

    Función para crear middleware en el servidor

--- Gorilla/main.go
+++ Gorilla/main.go
@@ -15,6 +15,13 @@ func main() {
 		fmt.Fprint(w, "¡Bienvenido al servidor Gorilla!")
 	})
 
+	r.Use(func (next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Println(r.Method, r.URL.Path)
+			next.ServeHTTP(w, r)
+		})
+	})
+
 	log.Println("Servidor gorilla en ejecución en http://localhost:8080")
 	http.ListenAndServe(":8080", r)
 }
